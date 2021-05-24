c42c8d0367d6db37910cc9f5bc985ccc31cd87ca
Xceldeveloper <56355878+Xceldeveloper@users.noreply.github.com>
2021-05-24 02:49:04 +0100

    ..

--- src/router/index.js
+++ src/router/index.js
@@ -6,10 +6,10 @@ Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/:username?",
+    path: "/",
     name: "index",
     component: index,
-     props:true
+    // props:true
   },
   {
     path: "/about",
